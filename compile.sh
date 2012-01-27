105d22b60d30166a4b456f8f393db5861f4a5917
Humbedooh <humbedooh@users.sf.net>
2012-01-27 21:14:18 +0100

    Updated the rudimentary compile script

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Compiling..."
-gcc    -c -g -fPIC  -MMD -MP -MF main.o.d -o main.o main.c  -I/usr/include/apr-1.0 
-gcc     -shared -o mod_plua.so -fPIC main.o -lluajit-5.1 -lapr-1
+gcc    -c -g -fPIC  -MMD -MP -MF mod_plua.o.d -o mod_plua.o mod_plua.c  -I/usr/include/apr-1.0 
+gcc     -shared -o mod_plua.so -fPIC mod_plua.o -lluajit-5.1 -lapr-1
 rm *.o*
 echo "All done!"
